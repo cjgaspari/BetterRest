2f51bf60d73565386ee02f2f4ae8b655dc761c45
CJ Gaspari <cjgaspari@users.noreply.github.com>
2021-07-14 09:35:38 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by CJ Gaspari on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
