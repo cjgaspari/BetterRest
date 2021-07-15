f22f2199fbef4016cffc2f367a366b3e74e3cca4
CJ Gaspari <cjgaspari@users.noreply.github.com>
2021-07-14 19:26:49 -0700

    Made adjustments to the UI

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date()
+    @State private var wakeUp = defaultWakeTime
     @State private var coffeeAmount = 1
     
     @State private var alertTitle = ""
@@ -19,33 +19,56 @@ struct ContentView: View {
     //this seems to be deprecated, need to figure out using init(configuration:)
     let model = SleepCalculator()
     
+    //make defaultWakeTime static so that it belongs to ContentView rather than a single instance of the struct.
+    //Does not rely on the existence of any other properties
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                    Text("\(sleepAmount, specifier: "%g") hours")
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
                 }
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper(value: $coffeeAmount, in: 1...20) {
-                    if coffeeAmount == 1 {
-                        Text("1 Cup")
-                    } else {
-                        Text("\(coffeeAmount) cups")
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
                     }
                 }
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    Stepper(value: $coffeeAmount, in: 1...20) {
+                        if coffeeAmount == 1 {
+                            Text("1 Cup")
+                        } else {
+                            Text("\(coffeeAmount) cups")
+                        }
+                    }
+                }
+                VStack() {
+                    Button(action: calculateBedtime) {
+                        Text("Calculate")
+                    }
+                }
+                
             }
             .navigationTitle("Better Rest")
-            .navigationBarItems(trailing:
-                Button(action: calculateBedtime) {
-                    Text("Calculate")
-                })
+//            .navigationBarItems(trailing:
+//                Button(action: calculateBedtime) {
+//                    Text("Calculate")
+//                })
             .alert(isPresented: $showingAlert, content: {
                 Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
             })
